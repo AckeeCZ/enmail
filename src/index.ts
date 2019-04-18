5392fc789e588ce29ecf87f2039befece04bc558
Štefan Prokop <sprokop1010@seznam.cz>
2019-04-18 08:00:11 +0200

    Update package exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { createOffice, instances, ServiceType } from './createOffice';
-import * as Mail from './Mail';
-import { GmailAuthType } from './offices/GmailOffice';
-import { compileLodashFileTemplate } from './templates/compileLodashFileTemplate';
-import * as compileLodashTemplate from './templates/compileLodashTemplate';
+import { get } from 'lodash';
+import { instances } from './createOffice';
+import { Office } from './offices/Office';
+
+export * from './createOffice';
+export * from './Mail';
+export * from './offices/FcmOffice';
+export * from './offices/GmailOffice';
+export * from './offices/OnesignalOffice';
+export * from './offices/Office';
+export * from './offices/SendgridOffice';
+export * from './templates/compileLodashFileTemplate';
+export * from './templates/compileLodashTemplate';
+export * from './templates/withDefaultContext';
 
 declare global {
     type Nullable<T> = T | null;
 }
 
-export = {
-    compileLodashFileTemplate,
-    compileLodashTemplate,
-    GmailAuthType,
-    Mail,
-    ServiceType,
-
-    createOffice,
-    getOffice: (ident: string) => instances.get(ident),
-};
+export const getWrappedOffice = (ident?: string) => instances.get(ident || 'default');
+export const getOffice = (ident?: string) => get(instances.get(ident || 'default'), 'office') as Office;
