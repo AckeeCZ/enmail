c2ce3877e75ae7206e17618a70357fca60b7625a
Štefan Prokop <sprokop1010@seznam.cz>
2019-06-07 08:53:40 +0200

    Change Gmail office options

--- src/offices/GmailOffice.ts
+++ src/offices/GmailOffice.ts
@@ -6,25 +6,38 @@ import {ServiceType} from '../createOffice';
 import { Mail, MailType } from '../Mail';
 import { Office } from './Office';
 
+type GmailAuthOptions = GmailLoginOptions | GmailOAuth2Options;
+
 export enum GmailAuthType {
     login = 'login',
     oauth2= 'oauth2',
 }
 
-export interface GmailOfficeOptions {
-    accessToken?: string;
-    accessUrl?: string;
-    clientId?: string;
-    clientSecret?: string;
-    expires?: number;
-    pass?: string;
-    privateKey?: string;
-    refreshToken?: string;
-    serviceClient?: string;
-    type: GmailAuthType;
+export interface GmailLoginOptions {
     user: string;
-    mailer?: any;
+    pass: string;
 }
+
+export interface GmailOAuth2Options {
+    user: string;
+    clientId: string;
+    clientSecret: string;
+    refreshToken: string;
+}
+
+export type GmailOfficeOptions = {
+    authType: GmailAuthType;
+    settings: GmailAuthOptions;
+    mailer?: any;
+} & (
+    | {
+    authType: GmailAuthType.login;
+    settings: GmailLoginOptions;
+}
+    | {
+    authType: GmailAuthType.oauth2;
+    settings: GmailOAuth2Options;
+});
 
 export class GmailOffice extends Office {
     public mailer!: MailerMail;
@@ -32,7 +45,10 @@ export class GmailOffice extends Office {
         const definedMailer = options.mailer || nodemailer;
         const mailer = definedMailer.createTransport({
             service: ServiceType.gmail,
-            auth: options,
+            auth: {
+                ...options.settings,
+                type: options.authType,
+            },
         } as SMTPPool.Options);
         super(mailer);
     }
@@ -44,7 +60,7 @@ export class GmailOffice extends Office {
             [mail.type === MailType.TEXT ? MailType.TEXT : MailType.HTML]: mail.content,
         }, mail.mailerOptions);
         return new Promise((resolve, reject) => {
-            this.mailer.sendMail(mailRequest, (err: Nullable<Error>, result: nodemailer.SentMessageInfo) =>
+            this.mailer.sendMail(mailRequest, (err: Error | null, result: nodemailer.SentMessageInfo) =>
                 (err ? reject(err) : resolve(result)));
         });
     }
