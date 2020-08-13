995069ee4aed17140057d13d147b7ea20b8230b3
Štefan Prokop <sprokop1010@seznam.cz>
2020-08-13 09:21:54 +0200

    ✨ Add nodemailer adapter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { createSendgrid } from './lib/adapters/SendgridAdapter';
+import { createNodemailer, NodemailerOptions } from './lib/adapters/NodemailerAdapter';
+import { createSendgrid, SendgridOptions } from './lib/adapters/SendgridAdapter';
 import { Adapter, AdapterSend, Message, SendMessage } from './lib/enmail';
 
 export {
@@ -16,10 +17,12 @@ const send: SendMessage = (message, sendFn, sendOptions) => {
 };
 const sender = <AdapterOptions, SendOptions, MailService>(adapter: Adapter<AdapterOptions, SendOptions, MailService>) => adapter.sender;
 
+export { NodemailerOptions, SendgridOptions };
 export default {
     send,
     sender,
     createAdapter: {
         sendgrid: createSendgrid,
+        nodemailer: createNodemailer,
     },
 };
