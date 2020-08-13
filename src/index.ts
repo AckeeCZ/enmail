bd5f0de96aa5848ce01fc9b7519e089002c5fbdf
Štefan Prokop <sprokop1010@seznam.cz>
2020-08-13 11:49:40 +0200

    ✨ dd & export message types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { createNodemailer, NodemailerOptions } from './lib/adapters/NodemailerAdapter';
-import { createSendgrid, SendgridOptions } from './lib/adapters/SendgridAdapter';
+import { createNodemailer, NodemailerMessage, NodemailerOptions } from './lib/adapters/NodemailerAdapter';
+import { createSendgrid, SendgridMessage, SendgridOptions } from './lib/adapters/SendgridAdapter';
 import { Adapter, AdapterSend, Message, SendMessage } from './lib/enmail';
 
 export {
@@ -17,7 +17,7 @@ const send: SendMessage = (message, sendFn, sendOptions) => {
 };
 const sender = <AdapterOptions, SendOptions, MailService>(adapter: Adapter<AdapterOptions, SendOptions, MailService>) => adapter.sender;
 
-export { NodemailerOptions, SendgridOptions };
+export { NodemailerMessage, NodemailerOptions, SendgridMessage, SendgridOptions };
 export default {
     send,
     sender,
--- src/lib/adapters/NodemailerAdapter.ts
+++ src/lib/adapters/NodemailerAdapter.ts
@@ -2,15 +2,16 @@ import * as nodemailer from 'nodemailer';
 import { Options } from 'nodemailer/lib/smtp-transport';
 import { Adapter, AdapterSend } from '../enmail';
 
+export type NodemailerMessage = nodemailer.SendMailOptions;
 export type NodemailerOptions = Options;
 
-export const createNodemailer = (adapterOptions: NodemailerOptions): Adapter<NodemailerOptions, Partial<nodemailer.SendMailOptions>> => {
+export const createNodemailer = (adapterOptions: NodemailerOptions): Adapter<NodemailerOptions, Partial<NodemailerMessage>> => {
     const sender = nodemailer.createTransport(adapterOptions);
     return {
         options: adapterOptions,
         mailService: sender,
         sender: (sendOptions) => {
-            const send: AdapterSend<Partial<nodemailer.SendMailOptions>> = (message, options) => {
+            const send: AdapterSend<Partial<NodemailerMessage>> = (message, options) => {
                 const { content, ...rest } = message;
                 const msg = {
                     ...rest,
