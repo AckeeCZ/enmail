4f5f7c374db45280e19e6d42c1a565f3d0aaeec9
Štefan Prokop <sprokop1010@seznam.cz>
2020-08-21 13:40:51 +0200

    🏷️ Add nodemailer mail service type

--- src/lib/adapters/NodemailerAdapter.ts
+++ src/lib/adapters/NodemailerAdapter.ts
@@ -1,11 +1,12 @@
 import * as nodemailer from 'nodemailer';
+import * as Mail from 'nodemailer/lib/mailer';
 import { Options } from 'nodemailer/lib/smtp-transport';
 import { Adapter, AdapterSend } from '../enmail';
 
 export type NodemailerMessage = nodemailer.SendMailOptions;
 export type NodemailerOptions = Options;
 
-export const createNodemailer = (adapterOptions: NodemailerOptions): Adapter<NodemailerOptions, Partial<NodemailerMessage>> => {
+export const createNodemailer = (adapterOptions: NodemailerOptions): Adapter<NodemailerOptions, Partial<NodemailerMessage>, Mail> => {
     const sender = nodemailer.createTransport(adapterOptions);
     return {
         options: adapterOptions,
